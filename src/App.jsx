f8d586f6c66274736fbcdd053b28c6d3aa3a5984
FachrielDzikri30 <ffadhilahdzikri@gmail.com>
2023-07-06 11:48:16 +0700

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react'
+import FoodForm from './components/FoodForm'
+import FoodList from './components/FoodList'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [foods, setFoods] = useState([])
+  
+    const addFood = (food) => {
+       setFoods([...foods, food])
+    }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    const deleteMenu = (index) => {
+      const updatedMenu = [...foods]
+      updatedMenu.splice(index, 1)
+      setFoods(updatedMenu)
+    }
+  
+    const totalCalories = foods.reduce((total, food) => total + food.calories,0)
+
+    return (
+      <div style={styles.container}>
+        <h1 style={styles.title}>Calories Calculator</h1>
+        <FoodForm addFood={addFood}/>
+        <FoodList deleteMenu={deleteMenu} foods={foods} />
+        <p style={styles.sum}>Total Calories : {totalCalories} kcals</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '12px',
+  },
+  title: {
+    fontSize:'36px',
+  },
+  sum: {
+    fontSize: '28px',
+    fontWeight:'bold',
+    marginTop:'1px',
+  },
 }
 
 export default App
