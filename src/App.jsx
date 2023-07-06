4dfa0184f4d2fe11eab8b7e6f28c2324b2026b20
FachrielDzikri30 <ffadhilahdzikri@gmail.com>
2023-07-06 12:34:19 +0700

    Add Configurations for Github Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,18 @@
-import React, {useState} from 'react'
-import FoodForm from './components/FoodForm'
-import FoodList from './components/FoodList'
+import React from 'react'
+import Menu from './components/Menu'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element:<Menu />,
+    },
+  ]
+  {basename: '/calories-calculator-app/'}
+)
 
 function App() {
-    const [foods, setFoods] = useState([])
-  
-    const addFood = (food) => {
-       setFoods([...foods, food])
-    }
-
-    const deleteMenu = (index) => {
-      const updatedMenu = [...foods]
-      updatedMenu.splice(index, 1)
-      setFoods(updatedMenu)
-    }
-  
-    const totalCalories = foods.reduce((total, food) => total + food.calories,0)
-
-    return (
-      <div style={styles.container}>
-        <h1 style={styles.title}>Calories Calculator</h1>
-        <FoodForm addFood={addFood}/>
-        <FoodList deleteMenu={deleteMenu} foods={foods} />
-        <p style={styles.sum}>Total Calories : {totalCalories} kcals</p>
-      </div>
-    )
-}
-
-const styles = {
-  container: {
-    textAlign: 'center',
-    padding: '12px',
-  },
-  title: {
-    fontSize:'36px',
-  },
-  sum: {
-    fontSize: '28px',
-    fontWeight:'bold',
-    marginTop:'1px',
-  },
+    return <RouterProvider router={router} />
 }
 
 export default App
